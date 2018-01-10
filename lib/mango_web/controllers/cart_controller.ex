53e071db7b2c965e8e4107a601d2c05ee6604e91
Thiam Hock Ng <nthock@gmail.com>
2018-01-10 22:22:54 +0800

    Complete cart items

--- lib/mango_web/controllers/cart_controller.ex
+++ lib/mango_web/controllers/cart_controller.ex
@@ -5,13 +5,8 @@ defmodule MangoWeb.CartController do
   def add(conn, %{"cart" => cart_params}) do
     cart = conn.assigns.cart
     case Sales.add_to_cart(cart, cart_params) do
-      {:ok, _} ->
-        %{"product_name" => name, "pack_size" => size, "quantity" => qty } = cart_params
-        message = "Product added to cart - #{name}(#{size}) x #{qty} qty"
-
-        conn
-        |> put_flash(:info, message)
-        |>redirect(to: page_path(conn, :index))
+      {:ok, cart} ->
+        render(conn, "add.json", cart: cart, cart_params: cart_params)
       {:error, _} ->
         conn
         |> put_flash(:info, "Error adding product to cart")
@@ -21,6 +16,21 @@ defmodule MangoWeb.CartController do
 
   def show(conn, _params) do
     cart = conn.assigns.cart
-    render conn, "show.html", cart: cart
+    cart_changeset = Sales.change_cart(cart)
+    render conn, "show.html", cart: cart, cart_changeset: cart_changeset
+  end
+
+  def update(conn, %{"cart" => cart_params}) do
+    cart = conn.assigns.cart
+    case Sales.update_cart(cart, cart_params) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Cart updated successfully")
+        |> redirect(to: cart_path(conn, :show))
+      {:error, _} ->
+        conn
+        |> put_flash(:info, "Error updating cart")
+        |> redirect(to: cart_path(conn, :show))
+    end
   end
 end
