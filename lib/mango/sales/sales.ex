d4f323dbb5f76bfb20c8feb54d91e8778a5c896c
Thiam Hock Ng <nthock@gmail.com>
2018-01-20 23:33:03 +0800

    Add checkout page and login verification

--- lib/mango/sales/sales.ex
+++ lib/mango/sales/sales.ex
@@ -26,6 +26,10 @@ defmodule Mango.Sales do
     update_cart(cart, attrs)
   end
 
+  def change_cart(%Order{} = order) do
+    Order.changeset(order, %{})
+  end
+
   def update_cart(cart, attrs) do
     cart
     |> Order.changeset(attrs)
--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -7,6 +7,10 @@ defmodule MangoWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+  end
+
+  pipeline :frontend do
+    # Add plugs related to frontend
     plug MangoWeb.Plugs.LoadCustomer
     plug MangoWeb.Plugs.FetchCart
   end
@@ -16,19 +20,30 @@ defmodule MangoWeb.Router do
   end
 
   scope "/", MangoWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :frontend] # Use the default browser stack
 
-    get "/", PageController, :index
-    get "/categories/:name", CategoryController, :show
-
+    # Add all routes that don't require authentication
     get "/register", RegistrationController, :new
     post "/register", RegistrationController, :create
     get "/login", SessionController, :new
     post "/login", SessionController, :create
-    get "/logout", SessionController, :delete
+
+    get "/", PageController, :index
+    get "/categories/:name", CategoryController, :show
+
     post "/cart", CartController, :add
     get "/cart", CartController, :show
+    patch "/cart", CartController, :update
     put "/cart", CartController, :update
+
+  end
+
+  # Authenticated scope
+  scope "/", MangoWeb do
+    pipe_through [:browser, :frontend, MangoWeb.Plugs.AuthenticateCustomer]
+
+    get "/logout", SessionController, :delete
+    get "/checkout", CheckoutController, :edit
   end
 
   # Other scopes may use custom stacks.
